b102f66a932e9b5f42ef20165c3b41459867d1cd
송윤석 <evon27@pothtonwer-MacBookAir.local>
2023-12-27 10:02:59 +0900

    create header skeleton

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const inter = IBM_Plex_Sans_KR({ weight: "400" });
+const inter = IBM_Plex_Sans_KR({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "netmap",
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,29 @@
 import styles from "./page.module.css";
+import { Poppins } from "next/font/google";
+
+import Image from "next/image";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "500" });
 
 export default function Lobby() {
-  return <main className={styles.main}></main>;
+  return (
+    <div className={styles.lobby}>
+      <header>
+        <div className={styles.logo_area}>
+          <Image
+            className={styles.logo}
+            src="/keeep.png"
+            alt="logo"
+            width={80}
+            height={80}
+          />
+          <h1 className={poppins.className}>netmap</h1>
+        </div>
+        <div className={styles.header_menu}>
+          <button>로그인</button>
+        </div>
+      </header>
+      <main></main>
+    </div>
+  );
 }
